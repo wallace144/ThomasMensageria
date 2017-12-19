d1b1de987417ec59be891cc77c4a75bd6daa49f2
William Fernando Suarez Bastidas <willfs144@gmail.com>
2017-12-19 14:27:14 -0500

    clase para la firma

--- app/src/main/java/co/udistrital/android/thomasmensageria/main/ui/MainActivity.java
+++ app/src/main/java/co/udistrital/android/thomasmensageria/main/ui/MainActivity.java
@@ -30,8 +30,8 @@ import co.udistrital.android.thomasmensageria.validate_route.ValidateRouteFragme
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
+    /*@BindView(R.id.fab)
+    FloatingActionButton fab;*/
     private FirebaseHelper helper;
 
 
@@ -47,14 +47,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
