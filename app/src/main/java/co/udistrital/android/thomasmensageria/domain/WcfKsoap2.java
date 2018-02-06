9a1072ef8043710f93f895007bf252c3c09ae8ac
William Fernando Suarez Bastidas <willfs144@gmail.com>
2018-02-06 16:53:02 -0500

    prueba ksoap

---
+++ app/src/main/java/co/udistrital/android/thomasmensageria/domain/WcfKsoap2.java
@@ -0,0 +1,8 @@
+package co.udistrital.android.thomasmensageria.domain;
+
+/**
+ * Created by wisuarez on 06/02/2018.
+ */
+
+public class WcfKsoap2 {
+}
---
+++ app/src/main/java/co/udistrital/android/thomasmensageria/login/PruebaRepositoryBorrar.java
@@ -0,0 +1,8 @@
+package co.udistrital.android.thomasmensageria.login;
+
+/**
+ * Created by wisuarez on 06/02/2018.
+ */
+
+public class PruebaRepositoryBorrar {
+}
